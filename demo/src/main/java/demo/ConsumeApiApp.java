d90651ac0f67bac5a9363aeb50a6828ffa4e7cb7
Rithwan <rithwanul@gmail.com>
2022-09-05 15:01:28 +0600

    Initial Commit

---
+++ demo/src/main/java/demo/ConsumeApiApp.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ConsumeApiApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeApiApp.class, args);
+	}
+}
